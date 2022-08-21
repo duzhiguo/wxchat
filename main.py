ea0c123941470bf7bf76c4a9b2acd8185e992804
duzhiguo <235608345@qq.com>
2022-08-21 19:46:36 +0800

    更改2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+import time
 
 today = datetime.now()
 # start_date = os.environ['START_DATE']
@@ -16,13 +17,30 @@ app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+def getday():
+  week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+  year=time.localtime().tm_year#既可以获取当前年份，也可以指定年份
+  month=time.localtime().tm_mon#既可以获取当前月份，也可以指定年月份
+  day=time.localtime().tm_mday#既可以获取当前天数，也可以指定天数
+  date =datetime.date(datetime(year=year,month=month,day=day))
+  now_time = time.localtime()
+  chinese_time = time.strftime('%Y年%m月%d日%H时%M分',now_time)
+  return chinese_time+" "+week_list[date.isoweekday()-1]
+
+
+
+today = getday()
+# start_date = os.environ['START_DATE']
+# birthday = os.environ['BIRTHDAY']
+
 
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  wen = "郑州 风力 :"+str(weather['wind'])+"\n最低温度 :"+str(weather['low'])+"\n最高温度 :"+str(weather['high'])+"\n空气质量 :"+str(weather['airQuality'])
+  return weather['weather'], wen
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -48,6 +66,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"data":{"value":today},"weather":{"value":wea},"temp":{"value":temperature},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"data":{"value":today+"\n"},"weather":{"value":wea+"\n"},"temp":{"value":temperature+"\n"},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
