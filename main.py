3c4cc2ce3e449af65fdc87604c6fbf38cb2b5ca4
duzhiguo <235608345@qq.com>
2022-08-23 21:56:13 +0800

    暂存

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import os
 import random
 import time
 
-today = datetime.now()
-# start_date = os.environ['START_DATE']
+
+
+
+# today = datetime.now()
+# # start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 # birthday = os.environ['BIRTHDAY']
 
@@ -17,19 +20,41 @@ app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+birthday = os.environ['BIRTHDAY']
+
+# today = datetime.now()
+# start_date = os.environ['START_DATE']
+# city = "郑州"
+# # birthday = os.environ['BIRTHDAY']
+
+# app_id = "wx2bbbf4a28d313c53"
+# app_secret = "7c88339aedf6ee1d88096504e5f77db2"
+
+# user_id = "oV51e5-x_wJ5qRFLJx8tMGYJNv_g"
+# template_id = "JL12uEZWl7ySB85zJQ9l4p3BzzxP0mE33dmuVWzVdyk"
+# birthday = "10-15"
+
+
+
 def getday():
-  week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
-  year=time.localtime().tm_year#既可以获取当前年份，也可以指定年份
-  month=time.localtime().tm_mon#既可以获取当前月份，也可以指定年月份
-  day=time.localtime().tm_mday#既可以获取当前天数，也可以指定天数
-  date =datetime.date(datetime(year=year,month=month,day=day))
-  now_time = time.localtime()
-  chinese_time = time.strftime('%Y年%m月%d日%H时%M分',now_time)
-  return chinese_time+" "+week_list[date.isoweekday()-1]
+  # week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+  # year=time.localtime().tm_year#既可以获取当前年份，也可以指定年份
+  # month=time.localtime().tm_mon#既可以获取当前月份，也可以指定年月份
+  # day=time.localtime().tm_mday#既可以获取当前天数，也可以指定天数
+  # date =datetime.date(datetime(year=year,month=month,day=day))
+  # now_time = time.localtime()
+  # chinese_time = time.strftime('%Y年%m月%d日%H时%M分',now_time) chinese_time+" "+week_list[date.isoweekday()-1]
+  url = "http://api.k780.com:88/?app=life.time&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json"
+  res = requests.get(url).json()
+  time = res["result"]["datetime_2"]
+  week = res["result"]["week_2"]
+
+  return str(time +" "+ week)
 
 
 
-today = getday()
+today1 = getday()
+today = datetime.now()
 # start_date = os.environ['START_DATE']
 # birthday = os.environ['BIRTHDAY']
 
@@ -48,6 +73,7 @@ def get_count():
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  print(next)
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
@@ -66,6 +92,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"data":{"value":today+"\n"},"weather":{"value":wea+"\n"},"temp":{"value":temperature+"\n"},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"data":{"value":today1+"\n"},"weather":{"value":wea+"\n"},"temp":{"value":temperature+"\n"},"birthday":{"value":str(get_birthday())},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
